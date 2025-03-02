f89fb6c085cf99f12a4c2e7984a320a3fadc2d8f
Alin Plamadeala <alin.plamadeala@lego.com>
2025-03-02 23:54:25 +0100

    openapi types npu-service

---
+++ libs/npu-service-openapi/README.md
@@ -0,0 +1,7 @@
+# npu-service-openapi
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build npu-service-openapi` to build the library.
---
+++ libs/npu-service-openapi/src/lib/types.ts
@@ -0,0 +1,7 @@
+import {components} from "./generated";
+
+export type CreateNpuRequestBody = components['schemas']['CreateNpuRequest'];
+export type UpdateNpuRequestBody = components['schemas']['UpdateNpuRequest'];
+
+
+export type NpuResponse = components['schemas']['NpuResponse'];

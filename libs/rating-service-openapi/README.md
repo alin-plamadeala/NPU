0b303998274b98478a7f685e0792d7dc648d11fd
Alin Plamadeala <alin.plamadeala@lego.com>
2025-03-03 01:24:35 +0100

    rating-service-openapi

---
+++ libs/rating-service-openapi/README.md
@@ -0,0 +1,7 @@
+# rating-service-openapi
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build rating-service-openapi` to build the library.
---
+++ libs/rating-service-openapi/src/lib/types.ts
@@ -0,0 +1,7 @@
+import {components} from "./generated";
+
+export type CreateRatingRequestBody = components['schemas']['CreateRatingRequest'];
+export type UpdateRatingRequestBody = components['schemas']['UpdateRatingRequest'];
+
+
+export type RatingResponse = components['schemas']['RatingResponse'];
